5b247403039ad83fd802d12b66ef5f4204cd8c77
Jerry <jerryhanksokafor@gmail.com>
2017-12-06 19:37:02 +0100

    feat : mock timeline

--- app/src/main/java/me/jerryhanks/stepviewapp/MainActivity.kt
+++ app/src/main/java/me/jerryhanks/stepviewapp/MainActivity.kt
@@ -1,20 +1,15 @@
 package me.jerryhanks.stepviewapp
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-import android.view.Gravity
+import android.support.v7.app.AppCompatActivity
 import android.view.View
-import android.view.ViewGroup
 import android.widget.FrameLayout
 import android.widget.TextView
-
-import java.util.ArrayList
-
+import kotlinx.android.synthetic.main.activity_main.*
 import me.jerryhanks.stepview.IndicatorAdapter
-import me.jerryhanks.stepview.TimeLineView
 import me.jerryhanks.stepview.interfaces.TimeLineViewCallback
 import me.jerryhanks.stepview.model.Status
-import me.jerryhanks.stepview.model.TimeLine
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -22,22 +17,21 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val timeLineView = findViewById<TimeLineView>(R.id.timeline_view)
+        val timeLineView = timelineView
 
         val timeLines = ArrayList<MyTimeLine>()
-        timeLines.add(MyTimeLine(Status.COMPLETED, "Sample Title 1", "Sample content 1"))
-        timeLines.add(MyTimeLine(Status.UN_COMPLETED, "Sample Title 2", "Sample contentlnldnfln 2sjnlf dfnldnlfndlnf dlflsnfndlnfd fdlfndnlfdf ldnflndlnfd fdlnfldnf dlnflndlfnld"))
-        timeLines.add(MyTimeLine(Status.COMPLETED, "Sample Title 3", "Sample content 3 kdkjsnxjnfs s f sf snfnslkdnflndlkfnd fdnlfndlnflndfldnflndlnf dfldnfndlnfldf dfnldnflndlnfd fndlnfdnlfd lfndnflndf dlnfldnlfnd fdnlfnd"))
-        timeLines.add(MyTimeLine(Status.COMPLETED, "Sample Title 4", "Sample content 4 sjdnlsnfndlnflndlfndllnfdnlfknd"))
-        timeLines.add(MyTimeLine(Status.ATTENTION, "Sample Title 5", "Sample content 5"))
-        timeLines.add(MyTimeLine(Status.COMPLETED, "Sample Title 6", "Sample content 6 dflndlnfndlnflnd dlnflndlnldnfl dlfndnlndlnldnlfndlnfldnlfndln"))
-        timeLines.add(MyTimeLine(Status.UN_COMPLETED, "Sample Title 7", "Sample content 7 sdpfjdfpdfjpdjpojdpofm;dfpmpmdpkdk[k[kdfm;dfm[l[dfl][kdf[omd[pkfkdkf'mdmf';mmvwejfopj2wpowmlskbokr"))
+        timeLines.add(MyTimeLine(Status.COMPLETED, getString(R.string.title_1), getString(R.string.content_1)))
+        timeLines.add(MyTimeLine(Status.UN_COMPLETED, getString(R.string.title_2), getString(R.string.content_2)))
+        timeLines.add(MyTimeLine(Status.COMPLETED, getString(R.string.title_3), getString(R.string.content_3)))
+        timeLines.add(MyTimeLine(Status.COMPLETED, getString(R.string.title_4), getString(R.string.content_4)))
+        timeLines.add(MyTimeLine(Status.ATTENTION, getString(R.string.title_5), getString(R.string.content_5)))
+        timeLines.add(MyTimeLine(Status.COMPLETED, getString(R.string.title_6), getString(R.string.content_6)))
 
 
         val adapter = IndicatorAdapter(timeLines, this, object : TimeLineViewCallback<MyTimeLine> {
             override fun onBindView(model: MyTimeLine, container: FrameLayout, position: Int): View {
                 val view = layoutInflater
-                        .inflate(me.jerryhanks.stepview.R.layout.sample_time_line,
+                        .inflate(R.layout.sample_time_line,
                                 container, false)
                 (view.findViewById<View>(R.id.tv_title) as TextView).text = model.title
                 (view.findViewById<View>(R.id.tv_content) as TextView).text = model.content
